987f17f90b2cece8aed50ef79618cc2f8b5d1f5c
Matthew Guizar <matthewrguizar@gmail.com>
2023-03-13 14:14:54 -0700

    added delete, insert and helper method for delete

--- bst.py
+++ bst.py
@@ -29,6 +29,20 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
 
+    def __r_insert(self, current_node, value):
+        if current_node is None:
+            return Node(value)
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value)
+        return current_node
+
+    def r_insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        self.__r_insert(self.root, value)
+
     def contains(self, value):
         if self.root is None:
             return False
@@ -55,15 +69,49 @@ class BinarySearchTree:
     def r_contains(self, value):
         return self.__r_contains(self.root, value)
 
+    def __delete_node(self, current_node, value):
+        if current_node is None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left is None and current_node.right is None:
+                return None
+            elif current_node.left is None:
+                current_node = current_node.right
+            elif current_node.right is None:
+                current_node = current_node.left
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node
+
+    def delete_node(self, value):
+        self.__delete_node(self.root, value)
+
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+
 
 my_tree = BinarySearchTree()
-my_tree.insert(47)
-my_tree.insert(21)
-my_tree.insert(76)
-my_tree.insert(18)
-my_tree.insert(27)
-my_tree.insert(52)
-my_tree.insert(82)
+# my_tree.insert(47)
+# my_tree.insert(21)
+# my_tree.insert(76)
+# my_tree.insert(18)
+# my_tree.insert(27)
+# my_tree.insert(52)
+# my_tree.insert(82)
 
-print(my_tree.r_contains(27))
-print(my_tree.r_contains(17))
+my_tree.r_insert(2)
+my_tree.r_insert(1)
+my_tree.r_insert(3)
+
+print(my_tree.root.value)
+print(my_tree.root.left.value)
+print(my_tree.root.right.value)
+
