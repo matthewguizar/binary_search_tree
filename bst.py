08999fde08d51a2c0be5373bb76c272f89022503
Matthew Guizar <matthewrguizar@gmail.com>
2023-03-13 17:46:59 -0700

    tree traversal

--- bst.py
+++ bst.py
@@ -92,19 +92,103 @@ class BinarySearchTree:
     def delete_node(self, value):
         self.__delete_node(self.root, value)
 
+    # starts a root and searches through each node at current depth
+    # before moving onto the next level of nodes
     def BFS(self):
+        # set current_node to the root of the tree
         current_node = self.root
+
+        # create an empty queue to store nodes to visit
         queue = []
+
+        # create an empty list to store the values of visited nodes
         results = []
+
+        # add the root node to the queue
         queue.append(current_node)
 
+        # continue until all nodes have been visited
         while len(queue) > 0:
+            # remove the first node from the queue
             current_node = queue.pop(0)
+
+            # add the value of the visited node to the results list
             results.append(current_node.value)
+
+            # if the visited node has a left child, add it to the queue
             if current_node.left is not None:
                 queue.append(current_node.left)
+
+            # if the visited node has a right child, add it to the queue
             if current_node.right is not None:
                 queue.append(current_node.right)
+
+        # return the list of visited node values
+        return results
+
+    # goes through entire depth of each node starting with left side
+    # finishing off with right and restarting with each node-level
+    def dfs_pre_order(self):
+        results = []  # create an empty list to store the values of visited nodes
+
+        def traverse(current_node):
+            # append the value of the current node to the results list
+            results.append(current_node.value)
+
+            # if the current node has a left child, recursively traverse it
+            if current_node.left is not None:
+                traverse(current_node.left)
+
+            # if the current node has a right child, recursively traverse it
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        # start the pre-order traversal from the root of the tree
+        traverse(self.root)
+
+        # return the list of visited node values
+        return results
+
+    def dfs_post_order(self):
+        results = []  # create an empty list to store the values of visited nodes
+
+        def traverse(current_node):
+            # if the current node has a left child, recursively traverse it
+            if current_node.left is not None:
+                traverse(current_node.left)
+
+            # if the current node has a right child, recursively traverse it
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+            # append the value of the current node to the results list
+            results.append(current_node.value)
+
+        # start the post-order traversal from the root of the tree
+        traverse(self.root)
+
+        # return the list of visited node values
+        return results
+
+    def dfs_in_order(self):
+        results = []  # create an empty list to store the values of visited nodes
+
+        def traverse(current_node):
+            # if the current node has a left child, recursively traverse it
+            if current_node.left is not None:
+                traverse(current_node.left)
+
+            # append the value of the current node to the results list
+            results.append(current_node.value)
+
+            # if the current node has a right child, recursively traverse it
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        # start the in-order traversal from the root of the tree
+        traverse(self.root)
+
+        # return the list of visited node values
         return results
 
     def min_value(self, current_node):
@@ -122,4 +206,4 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
-print(my_tree.BFS())
+print(my_tree.dfs_pre_order())
